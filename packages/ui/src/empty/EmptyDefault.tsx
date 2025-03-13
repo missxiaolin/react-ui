c1d5c4d77a1f173a7d64e8dcb2be9364058423a4
missxiaolin <462441355@qq.com>
2025-03-13 14:59:04 +0800

    fix: 报错解决

--- packages/ui/src/empty/EmptyDefault.tsx
+++ packages/ui/src/empty/EmptyDefault.tsx
@@ -10,15 +10,7 @@ export interface EmptyDefaultProps extends componentsPops {
 const EmptyDefault: React.FC<EmptyDefaultProps> = props => {
     const { width = 127 } = props
     return (
-        <svg
-            t="1741847399288"
-            class="icon"
-            viewBox="0 0 1024 1024"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            p-id="4844"
-            width={width}
-        >
+        <svg className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4844" width={width}>
             <path
                 d="M855.8 814.2H386.3c-24.2 0-43.8-19.6-43.8-43.8V120c0-24.2 19.6-43.8 43.8-43.8H866c24.2 0 43.8 19.6 43.8 43.8v640.2c-0.1 29.8-24.2 54-54 54z"
                 fill="#F2F7F7"
